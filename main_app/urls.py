c50f7dfad634ec61dcfa742a87ca472c85b61476
cathygarciapinon <cathygarciapinon@gmail.com>
2022-11-22 10:39:57 -0800

    Edit and Delete

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('accounts/signup/', views.signup, name='signup'),
     path('destinations/<int:destination_id>/', views.destination_details, name='detail'),
     path('destinations/create/', views.DestinationCreate.as_view(), name='destinations_create'),
+    path('destinations/<int:pk>/update/', views.DestinationUpdate.as_view(), name='destinations_update'),
+    path('destinations/<int:pk>/delete/', views.DestinationDelete.as_view(), name='destinations_delete'),
 ]
