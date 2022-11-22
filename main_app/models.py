6255fc25ebcf0429c86a45a9a225348944620c43
Phillip Black <phil@Phillips-MacBook-Air.local>
2022-11-22 11:20:18 -0600

    destination form functionality

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -6,6 +7,12 @@ class Destination(models.Model):
   place = models.CharField(max_length=100)
   description = models.TextField(max_length=300)
   user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"{self.place}"
+
+  def get_absolute_url(self):
+    return reverse('detail', kwargs={'destination_id': self.id})
 
 class Photo(models.Model):
   url = models.CharField(max_length=200)
--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     # signup url
     path('accounts/signup/', views.signup, name='signup'),
+    path('destinations/<int:destination_id>/', views.destination_details, name='detail'),
+    path('destinations/create/', views.DestinationCreate.as_view(), name='destinations_create'),
 ]
